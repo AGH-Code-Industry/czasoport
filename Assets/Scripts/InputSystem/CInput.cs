ca48958cfe635dce4295212362ab5d424352d76a
Patryk Lesiak <patryklesiak@student.agh.edu.pl>
2023-07-21 22:35:50 +0200

    Created actions and generated C# action script

---
+++ Assets/Scripts/InputSystem/CInput.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InputSystem {
+    public class CInput {
+        private static InputActions _inputActions;
+        
+        static CInput() {}
+    }
+}
+
