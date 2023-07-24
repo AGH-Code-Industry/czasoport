0dea0b1c2569fdac16b20fecbd7b963b6a3749eb
Patryk Lesiak <patryklesiak@student.agh.edu.pl>
2023-07-24 21:02:56 +0200

    Comment on ApplicationSettingsSO

--- Assets/Scripts/Settings/Developer/ApplicationSettingsSO.cs
+++ Assets/Scripts/Settings/Developer/ApplicationSettingsSO.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 namespace Settings.Developer {
     [CreateAssetMenu(fileName = "ApplicationSettings", menuName = "Settings/DeveloperSettings/ApplicationSettings")]
+    // Settings for entire application that defines variables for startup and core processes
     public class ApplicationSettingsSO : ScriptableObject {
         [Tooltip("Name of the scene that loads global objects at the start of the application.")]
         public string globalDataSceneName;
