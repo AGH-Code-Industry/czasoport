b6a597b72576fcc34b735e80f37f87f8f63201ee
Gilbert Guszcza <guszczagilbert@gmail.com>
2023-07-25 10:37:14 +0200

    Animation
    Prosta animacja zmiany czasu

--- Assets/Scripts/TimeChange/TimeChanger.cs
+++ Assets/Scripts/TimeChange/TimeChanger.cs
@@ -13,13 +13,22 @@ public class TimeChanger : MonoBehaviour{
     private InputActions InputActions;
     private Transform PlayerTransform;
 
-    public enum Time : int{
+    [SerializeField]private Animator animator;
+    private delegate void changeTime();
+    private changeTime TCT;
+    [SerializeField]private float timeToChange = 0.3f;
+    private float counterToChange;
+    private TimeMachine new_id;
+    private int change;
+    
+
+    public enum TimeMachine : int{
         Past = 0,
         Present = 1,
         Future = 2
     }
 
-    public Time actualTime = Time.Present;
+    public TimeMachine actualTime = TimeMachine.Present;
 
     private void Start(){
         boxes = new List<CheckCollider>();
@@ -41,6 +50,11 @@ public class TimeChanger : MonoBehaviour{
 
         InputActions.Teleport.TeleportBack.performed += TimeBack;
         InputActions.Teleport.TeleportForward.performed += TimeForward;
+        counterToChange = timeToChange;
+    }
+
+    private void Update() {
+        if(TCT != null)TCT();
     }
 
     private void OnDisable(){
@@ -48,16 +62,49 @@ public class TimeChanger : MonoBehaviour{
         InputActions.Teleport.TeleportForward.performed -= TimeForward;
     }
 
-    private void TimeBack(InputAction.CallbackContext ctx){ChangeTime(-1);}
-    private void TimeForward(InputAction.CallbackContext ctx){ChangeTime(1);}
+    private void TimeBack(InputAction.CallbackContext ctx){
+        change = -1;
+        TCT += TryChange;
+        InputActions.Teleport.TeleportBack.performed -= TimeBack;
+        InputActions.Teleport.TeleportForward.performed -= TimeForward;
+    }
 
-    private void ChangeTime(int change){
+    private void TimeForward(InputAction.CallbackContext ctx){
+        change = 1;
+        TCT += TryChange;
+        InputActions.Teleport.TeleportBack.performed -= TimeBack;
+        InputActions.Teleport.TeleportForward.performed -= TimeForward;
+    }
+
+    private void TryChange(){
         if(actualTime == 0 && change == -1) change = 2;
-        Time new_id = (Time)(((int)actualTime+change)%3);
+        new_id = (TimeMachine)(((int)actualTime+change)%3);
         //CDebug.Log(new_id,Colorize.Magenta);
-        if(!CanChangeTime(new_id-actualTime)) return;
-        _transform.Translate(TimeJump * (int)(new_id-actualTime));
-        actualTime = new_id;
+        change = 0;
+        if(CanChangeTime(new_id-actualTime)){
+            animator.SetTrigger("Start");
+            TCT += ChangeTime;
+        }
+        else{
+            InputActions.Teleport.TeleportBack.performed += TimeBack;
+            InputActions.Teleport.TeleportForward.performed += TimeForward;
+        }
+        TCT -= TryChange;
+    }
+
+    private void ChangeTime(){
+        counterToChange -= Time.deltaTime;
+        if(counterToChange < 0f){
+            _transform.Translate(TimeJump * (int)(new_id-actualTime));
+            actualTime = new_id;
+            animator.SetTrigger("End");
+            
+            counterToChange = timeToChange;
+            
+            InputActions.Teleport.TeleportBack.performed += TimeBack;
+            InputActions.Teleport.TeleportForward.performed += TimeForward;
+            TCT -= ChangeTime;
+        }
     }
 
     private bool CanChangeTime(int when){
