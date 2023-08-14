bf24d2410246c073efc21018ce18f016cb2c8124
Kacper Sobolak <kacpersobolak03@gmail.com>
2023-08-14 17:16:18 +0200

    clean up

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -1,25 +1,30 @@
 using Settings;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class Menu : MonoBehaviour
-{
-    [SerializeField] private Button playButton;
-    [SerializeField] private Button quitButton;
+namespace UI {
+	/// <summary>
+	/// Script for main menu in game
+	/// </summary>
+	public class Menu : MonoBehaviour {
+		[SerializeField] private Button playButton;
+		[SerializeField] private Button quitButton;
 
-    void Awake()
-    {
-        playButton.onClick.AddListener(PlayAction);
-        quitButton.onClick.AddListener(() => UnityEngine.Application.Quit());
-    }
+		void Awake() {
+			playButton.onClick.AddListener(PlayAction);
+			quitButton.onClick.AddListener(() => UnityEngine.Application.Quit());
+		}
 
-    void PlayAction() {
-		SceneManager.LoadSceneAsync(DeveloperSettings.Instance.appSettings.sceneToLoadName, LoadSceneMode.Additive);
-        SceneManager.UnloadSceneAsync(gameObject.scene);
+		/// <summary>
+		/// Action called when user press Play button
+		/// </summary>
+		void PlayAction() {
+			SceneManager.LoadSceneAsync(DeveloperSettings.Instance.appSettings.sceneToLoadName, LoadSceneMode.Additive);
+			SceneManager.UnloadSceneAsync(gameObject.scene);
+		}
 	}
-
-
 }
