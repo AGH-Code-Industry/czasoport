fe4cd7a7c3501f221f7a5178cbe75bd847af9660
Kacper Sobolak <kacpersobolak03@gmail.com>
2023-08-15 18:30:26 +0200

    cleanup

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -1,7 +1,4 @@
 using Settings;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
