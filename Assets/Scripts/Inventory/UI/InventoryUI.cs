f1a07c89fc7a53b4ed853b7c450a8d5947b8744b
Gilbert Guszcza <guszczagilbert@gmail.com>
2023-08-16 02:54:33 +0200

    Update documentation

--- Assets/Scripts/Inventory/UI/InventoryUI.cs
+++ Assets/Scripts/Inventory/UI/InventoryUI.cs
@@ -33,7 +33,7 @@ namespace Inventory.UI {
         }
 
         /// <summary>
-        /// Highlight choosed slot.
+        /// Highlight choosed slot or/and set durabilty for active slot.
         /// </summary>
         /// <param name="newId"></param>
         public void OnItemStateChange(int id, int new_durability = -1) {
@@ -42,13 +42,13 @@ namespace Inventory.UI {
                 _activeId = id;
                 _slots[_activeId].Active();
             }
-            else if (new_durability != -1){
+            if (new_durability != -1){
                 _slots[id].SetDurabilty(new_durability);
             }
         }
 
         /// <summary>
-        /// Change slot's image to newItem.
+        /// Change slot's image to newItem and set durability.
         /// </summary>
         /// <param name="id"></param>
         /// <param name="newItem"></param>
@@ -58,7 +58,7 @@ namespace Inventory.UI {
         }
 
         /// <summary>
-        /// Change slot's image.apha to 0.
+        /// Change slot's image.apha to 0 and set text responsible for durability to "".
         /// </summary>
         /// <param name="id"></param>
         public void OnItemRemoved(int id) {
