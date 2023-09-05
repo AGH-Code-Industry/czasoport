eb41a77cda12ee9406de30738a2297170dca54bd
Patryk Lesiak <patryklesiak@student.agh.edu.pl>
2023-09-05 20:54:12 +0200

    Changed events handling

---
+++ Assets/Scripts/Inventory/EventArguments/ItemRemovedEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Items;
+
+namespace Inventory.EventArguments {
+    public class ItemRemovedEventArgs : EventArgs {
+        // Slot from which item was removed
+        public int Slot;
+        // Removed item
+        public Item Item;
+    }
+}
---
+++ Assets/Scripts/Inventory/EventArguments/ItemStateChangedEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using Items;
+
+namespace Inventory.EventArguments {
+    public class ItemStateChangedEventArgs {
+        // Slot that hold the item
+        public int Slot;
+        // Item which state has been changed
+        public Item Item;
+    }
+}
