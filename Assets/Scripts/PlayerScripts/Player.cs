e3567b260e851f1ace8aa35861329f86163c74aa
Mikosztyla <mikosztyla@gmai.com>
2023-10-10 10:45:06 +0200

    Walking and idle animations

--- Assets/Scripts/PlayerScripts/Player.cs
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using UnityEngine;
 using CustomInput;
 using Settings;
@@ -8,6 +8,7 @@ namespace PlayerScripts {
         public static Player Instance = null;
 
         private PlayerSettingsSO _settings;
+        Animator _animator;
 
         private void Awake() {
             if (Instance == null) {
@@ -20,6 +21,7 @@ namespace PlayerScripts {
 
         private void Start() {
             _settings = DeveloperSettings.Instance.playerSettings;
+            _animator = GetComponent<Animator>();
         }
 
         private void OnDestroy() {
@@ -30,7 +32,15 @@ namespace PlayerScripts {
 
         private void Update() {
             var movement = CInput.NavigationAxis;
+            _animator.SetFloat("Horizontal", movement.x);
+            _animator.SetFloat("Vertical", movement.y);
+            float _speed = movement.sqrMagnitude;
+            _animator.SetFloat("Speed", _speed);
+            if (_speed > 0.01) {
+                _animator.SetFloat("LastHorizontal", movement.x);
+                _animator.SetFloat("LastVertical", movement.y);
+            }
             transform.Translate(Time.deltaTime * _settings.movementSpeed * new Vector2(movement.x, movement.y));
-        }
+       }
     }
 }
