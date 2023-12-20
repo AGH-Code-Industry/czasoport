659f0fefcca55be39b85e34d050bdb1d3f3146b9
Liero <dawidmazi@student.agh.edu.pl>
2023-12-20 21:18:39 +0100

    Merge tutorial and post tutorial layout

--- Assets/Scripts/Utils/Attributes/ReadOnlyAttribute.cs
+++ Assets/Scripts/Utils/Attributes/ReadOnlyAttribute.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace Utils.Attributes {
     public class ReadOnlyAttribute : PropertyAttribute { }
 
-    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    /*[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
     public class ReadOnlyDrawer : PropertyDrawer {
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
             return EditorGUI.GetPropertyHeight(property, label, true);
@@ -16,5 +16,5 @@ namespace Utils.Attributes {
             EditorGUI.PropertyField(position, property, label, true);
             GUI.enabled = true;
         }
-    }
+    }*/
 }
