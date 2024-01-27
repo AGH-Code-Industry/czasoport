3940b198f9844857c0c4ed042e064de4ce86bdb1
Mikosztyla <mikosztyla@gmail.com>
2024-01-27 16:23:51 +0100

    Upgraded notification system

---
+++ Assets/Scripts/Notifications/NotificationUIController.cs
@@ -0,0 +1,24 @@
+﻿using Notifications;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class NotificationUIController : MonoBehaviour {
+    [SerializeField] GameObject _notificationHistoryPanel;
+    [SerializeField] GameObject _notificationWindow;
+    
+    public void OpenNotificationPanel() {
+        _notificationHistoryPanel.SetActive(true);
+        _notificationWindow.SetActive(false);
+    }
+
+    public void CloseNotificationPanel() {
+        _notificationHistoryPanel.SetActive(false);
+        _notificationWindow.SetActive(true);
+    }
+
+   
+}
