d5c60e83f7951aed9f6eaa532c2b52fdb1871f07
Mikołaj Gosztyła <115586050+Mikosztyla@users.noreply.github.com>
2024-01-27 16:30:53 +0100

    Feature/notifications system (#68)
    * Notification system added
    
    * Notification system changed
    
    * Accesibility fixed
    
    * New class added
    
    Notification class was added with some properties easy to change.
    In order to use notification system, add the "notification system" object to the scene and modify it's variables with the specific object from the "NotificationWindow" object, which should be added to the Canvas.
    
    * Upgraded notification system
    
    ---------
    
    Co-authored-by: Mikosztyla <mikosztyla@gmai.com>

--- Assets/Scripts/Notifications/NotificationManager.cs
+++ Assets/Scripts/Notifications/NotificationManager.cs
@@ -5,8 +5,10 @@ using Notifications;
 using UnityEngine;
 using TMPro;
 using Settings;
+using DataPersistence;
+using UnityEngine.UI;
 
-public class NotificationManager : MonoBehaviour {
+public class NotificationManager : MonoBehaviour, IDataPersistence {
 
     /* If you want to display a notification use this formula:
      NotificationManager.Instance.RaiseNotification(new Notification(string message, float displayTime));
@@ -15,9 +17,15 @@ public class NotificationManager : MonoBehaviour {
     public static NotificationManager Instance;
 
     [SerializeField]
-    GameObject _notificationWindow;
-    [SerializeField]
     TMP_Text _notificationMessage;
+    [SerializeField]
+    GameObject _scrollContent;
+    [SerializeField]
+    GameObject _logPrerab;
+
+    Queue<Notification> _notificationsToDisplay = new Queue<Notification>();
+    List<Notification> _notificationsHistory = new List<Notification>();
+    bool _isNotificationDisplaying = false;
 
     private void Start() {
         if (Instance == null) {
@@ -31,13 +39,47 @@ public class NotificationManager : MonoBehaviour {
         if (String.IsNullOrEmpty(notification.message)) {
             return;
         }
+        _notificationsToDisplay.Enqueue(notification);
+        if (!_isNotificationDisplaying ) {
+            _isNotificationDisplaying = true;
+            StartCoroutine(DisplayNotification());
+        }
+    }
+    
+    IEnumerator DisplayNotification() {
+        if (_notificationsToDisplay.Count == 0) {
+            _isNotificationDisplaying = false;
+            _notificationMessage.gameObject.SetActive(false);
+            yield break;
+        }
+        Notification notification = _notificationsToDisplay.Dequeue();
         _notificationMessage.text = notification.message;
-        _notificationWindow.SetActive(true);
-        Invoke("DisableNotificationWindow", notification.displayTime);
+        _notificationMessage.gameObject.SetActive(true);
+        _notificationsHistory.Add(notification);
+        UpdateLogs();
+        yield return new WaitForSeconds(notification.displayTime);
+        StartCoroutine(DisplayNotification());
     }
 
-    void DisableNotificationWindow() {
-        _notificationWindow.SetActive(false);
+    public void LoadPersistentData(GameData gameData) {
+        _notificationsHistory = new List<Notification>(gameData.notificationGameData.notificationHistory);
     }
 
+    public void SavePersistentData(ref GameData gameData) {
+        gameData.notificationGameData.notificationHistory = _notificationsHistory;
+    }
+
+    void UpdateLogs() {
+        for (int i = 0; i < _scrollContent.transform.childCount; i++) {
+            Destroy(_scrollContent.transform.GetChild(i).gameObject);
+        }
+        int it = 0;
+        foreach (Notification notification in _notificationsHistory) {
+            GameObject go = Instantiate(_logPrerab, _scrollContent.transform);
+            go.transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = notification.message.ToString();
+            if (it % 2 == 1) go.transform.GetChild(0).gameObject.GetComponent<Image>().color = Color.gray;
+            it += 1;
+        }
+        LayoutRebuilder.ForceRebuildLayoutImmediate(_scrollContent.GetComponent<RectTransform>());
+    }
 }
