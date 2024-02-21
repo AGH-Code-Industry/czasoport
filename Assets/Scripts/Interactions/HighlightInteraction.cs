8bffa4a18c8a53d3bea9930334324164069cb36d
Gilbert Guszcza <guszczagilbert@gmail.com>
2024-02-21 22:43:05 +0100

    idk

--- Assets/Scripts/Interactions/HighlightInteraction.cs
+++ Assets/Scripts/Interactions/HighlightInteraction.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using Interactions.Interfaces;
 using UnityEngine;
+using System.Collections;
 
 namespace Interactions {
     public class HighlightInteraction : MonoBehaviour, IHighlightable {
 
         private SpriteRenderer _sprite;
         private Material _material;
+        private bool _isFocused = false;
+        private float _animationTime = 0.3f;
+        private float _animationTimer;
 
         private void Start() {
             _sprite = GetComponent<SpriteRenderer>();
@@ -18,18 +22,31 @@ namespace Interactions {
         }
 
         public void EnableHighlight() {
-            _material.SetFloat("_OnOff",0f);
-            //_material.SetColor("_color",Color.red);
+            if (_isFocused) StartCoroutine(ToogleHighlight());
         }
 
         public void EnableFocusedHighlight() {
-            _material.SetFloat("_OnOff",1f);
-            //_material.SetColor("_color",Color.green);
+            if (!_isFocused) StartCoroutine(ToogleHighlight());
         }
 
         public void DisableHighlight() {
-            _material.SetFloat("_OnOff",0f);
-            //_material.SetColor("_color",Color.white);
+            if (_isFocused) StartCoroutine(ToogleHighlight());
+        }
+        
+        private IEnumerator ToogleHighlight() {
+			_isFocused = !_isFocused;
+            float targetAlpha = 1f - _material.GetFloat("_OnOff");
+			float startAlpha = _material.GetFloat("_OnOff");;
+            _animationTimer = _animationTime;
+                
+            while (_animationTimer > 0f) {
+            	_animationTimer -= Time.deltaTime;
+                yield return null;
+                _material.SetFloat("_OnOff",
+                	Mathf.Lerp(startAlpha, targetAlpha,
+					(_animationTime - _animationTimer) / _animationTime));
+            }
+            _material.SetFloat("_OnOff", targetAlpha);
         }
     }
 }
