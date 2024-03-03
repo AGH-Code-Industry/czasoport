bfd50d96966eb39804661b2eda20695c0a4b5d94
KacperSobolak <kacpersobolak03@gmail.com>
2024-03-03 13:10:16 +0100

    tape player (#88)

---
+++ Assets/Scripts/InteractableObjectSystem/Objects/TapePlayer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Interactions;
+using Items;
+using NPC;
+using UnityEngine;
+
+namespace InteractableObjectSystem.Objects {
+    [RequireComponent(typeof(BoxCollider2D))]
+    [RequireComponent(typeof(HighlightInteraction))]
+    public class TapePlayer : InteractableObject {
+        [SerializeField] private ItemSO _interactedWith;
+        [SerializeField] private List<PathWalking> _NPCPathWalkings = new ();
+
+        public override bool InteractionItem(Item item) {
+            if (item.ItemSO == _interactedWith) {
+                PlayMusic();
+                return true;
+            }
+
+            return false;
+        }
+
+        private void PlayMusic() {
+            foreach (var pathWalking in _NPCPathWalkings) {
+                pathWalking.StartWalk();
+            }
+        }
+    }
+}
