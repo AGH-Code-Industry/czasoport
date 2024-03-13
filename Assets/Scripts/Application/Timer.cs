74c46d8eb0f4f00d6f373096a18217333d2580e9
KacperSobolak <kacpersobolak03@gmail.com>
2024-03-13 23:48:01 +0100

    timer

---
+++ Assets/Scripts/Application/Timer.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+    public static Timer instance;
+
+    private void Awake() {
+        instance = this;
+    }
+
+    private float _elapsedTime = 0f;
+    private bool _timerOn = false;
+
+    public void StartTimer() {
+        _timerOn = true;
+    }
+
+    public void StopTimer() {
+        _timerOn = false;
+    }
+
+    private void ResetTimer()
+    {
+        _elapsedTime = 0f;
+    }
+
+    public float GetMeasuredTime()
+    {
+        return _elapsedTime;
+    }
+
+    void Update()
+    {
+        if (_timerOn)
+        {
+            _elapsedTime += Time.unscaledDeltaTime;
+        }
+    }
+}
