57689aae1b55a354ad5065cbda870f4e53345899
KacperSobolak <kacpersobolak03@gmail.com>
2024-03-14 12:58:22 +0100

    taktaktak

--- Assets/Scripts/UI/TimeUIToogle.cs
+++ Assets/Scripts/UI/TimeUIToogle.cs
@@ -6,14 +6,22 @@ namespace  UI {
         [SerializeField] private Sprite active;
         [SerializeField] private Sprite disactive;
         private Image _image;
+        [SerializeField] private bool tak;
 
         private void Awake() {
             _image = GetComponent<Image>();
         }
 
         public void SetStroke(bool isActive) {
-            if (isActive) _image.sprite = active;
-            else _image.sprite = disactive;
+            if (isActive) {
+                _image.sprite = active;
+                if (tak)
+                    _image.color = Color.black;
+            }
+            else {
+                _image.sprite = disactive;
+                _image.color = Color.white;
+            }
         }
     }
 }
