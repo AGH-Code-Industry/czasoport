0354127f36a375860034dee988f9f3fdabdc0c03
KacperSobolak <kacpersobolak03@gmail.com>
2024-03-14 12:44:15 +0100

    wazne

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -42,6 +42,8 @@ namespace Items
                     this
                     );
             }
+
+            LeanTween.scale(this.gameObject, new Vector3(1.2f, 1.2f, 1f), 0.4f).setLoopPingPong();
             if (Durability == 0) Durability = ItemSO.durability;
         }
 
