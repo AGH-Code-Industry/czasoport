c488e32c7f5624d72c3469a19d0c510f2e35a631
Gilbert Guszcza <guszczagilbert@gmail.com>
2024-04-11 09:07:07 +0200

    update highlight

--- Assets/Scripts/Interactions/HighlightInteraction.cs
+++ Assets/Scripts/Interactions/HighlightInteraction.cs
@@ -5,7 +5,7 @@ using System.Collections;
 
 namespace Interactions {
     public class HighlightInteraction : MonoBehaviour, IHighlightable {
-
+        public float scale = 0.02f;
         private SpriteRenderer _sprite;
         private Material _material;
         
@@ -22,7 +22,7 @@ namespace Interactions {
             _material = _sprite.material;
             _material.SetTexture("_texture", _sprite.sprite.texture);
             _material.SetFloat("_OnOff",0f);
-            _material.SetFloat("_scale",0.01f);
+            _material.SetFloat("_scale",scale);
             _material.SetColor("_color", Color.red);
 			_fadeIn = TurnHighlight(0f, 1f);
 			_fadeOut = TurnHighlight(1f, 0f);
