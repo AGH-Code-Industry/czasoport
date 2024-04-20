d50f289a388568cc95583ad085af9f4fd6da108d
Mikosztyla <mikosztyla@gmail.com>
2024-04-20 16:39:34 +0200

    Photocopier UI

---
+++ Assets/Scripts/UI/Items/PhotocotierUI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Interactions;
+using System;
+
+public class PhotocotierUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _documentCounter;
+
+    private void Start() {
+        _documentCounter.gameObject.SetActive(false);
+        this.gameObject.GetComponent<HighlightInteraction>().onHighlightStart += displayText;
+        this.gameObject.GetComponent<HighlightInteraction>().onHighlightEnd += stopDisplayText;
+    }
+
+    private void stopDisplayText(object sender, InteractionHighlightEventArgs e) {
+        if (e.senderObject.Equals(this.gameObject)) _documentCounter.gameObject.SetActive(false);
+    }
+
+    private void displayText(object sender, InteractionHighlightEventArgs e) {
+        if (e.senderObject.Equals(this.gameObject)) _documentCounter.gameObject.SetActive(true);
+    }
+
+    public void setCounter(String counterText) {
+        _documentCounter.text = counterText;
+    }
+}
