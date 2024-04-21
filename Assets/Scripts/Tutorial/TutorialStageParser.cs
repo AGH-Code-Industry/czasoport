c2752259b6e5e6143fd80e060eae72ad8cc0a732
Mikołaj Gosztyła <115586050+Mikosztyla@users.noreply.github.com>
2024-04-21 19:22:41 +0200

    Hotfix/tutorial manager refactor (#167)
    * Tutorial refactor
    
    * Tutorial refactoring done
    
    * Bug fix
    
    * dotnet
    
    * Method names fix
    
    ---------
    
    Co-authored-by: KacperSobolak <kacpersobolak03@gmail.com>

---
+++ Assets/Scripts/Tutorial/TutorialStageParser.cs
@@ -0,0 +1,9 @@
+using Ink.Parsed;
+using System.Collections.Generic;
+
+public class TutorialStageParser {
+    public string action;
+    public string conditionSatisfied;
+    public List<string> tutorialNotification;
+    public string specialCondition;
+}
