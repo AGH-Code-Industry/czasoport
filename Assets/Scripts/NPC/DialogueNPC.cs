10dec22e15837ebeadf378d08d55c3c94199b3a5
Gilbert Guszcza <guszczagilbert@gmail.com>
2024-07-13 10:19:30 +0200

    add afterdialog variable

--- Assets/Scripts/NPC/DialogueNPC.cs
+++ Assets/Scripts/NPC/DialogueNPC.cs
@@ -1,15 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Dialogues;
 using InteractableObjectSystem;
 using Items;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace NPC {
     public class DialogueNPC : InteractableObject {
         [SerializeField] private TextAsset dialogue;
+        [SerializeField] private UnityEvent afterDialog = null;
         public override void InteractionHand() {
-            DialogueManager.I.StartDialogue(dialogue);
+            DialogueManager.I.StartDialogue(dialogue,afterDialog.Invoke);
         }
 
         public override bool InteractionItem(Item item) {
