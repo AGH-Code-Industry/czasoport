4fcc74041cae185318e9167ecedac323909145a6
Gilbert Guszcza <guszczagilbert@gmail.com>
2024-09-12 21:50:36 +0200

    tutorial saving id's (#206)

---
+++ Assets/Scripts/DataPersistence/DataTypes/InteractableData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using InteractableObjectSystem.Objects;
+using UnityEngine;
+
+namespace DataPersistence.DataTypes {
+    public class InteractableData :  Data {
+        [Serializable]
+        public class InteractableSubData {
+            public int state;
+        }
+
+        public InteractableSubData data;
+
+        public override void SerializeInheritance() {
+            inheritanceJson = JsonUtility.ToJson(data);
+        }
+
+        public override void DeserializeInheritance() {
+            data = JsonUtility.FromJson<InteractableSubData>(inheritanceJson);
+        }
+    }
+
+}
