d7096ae9ea27be115cb9958bc50d6dfe72bb233a
Mikołaj Gosztyła <115586050+Mikosztyla@users.noreply.github.com>
2024-10-03 23:06:16 +0200

    Feature/new market graphics (#229)
    * new assets
    
    * new stragans added
    
    * market fixes
    
    * hammer durability
    
    * Light under every item
    
    * new npcs animations
    
    Added animation of all remaining NPCs from front view
    
    * Basic npc, npcs working now
    
    ---------
    
    Co-authored-by: ZariaRiv <kasia.kleczek@poczta.onet.pl>

---
+++ Assets/BasicNPC.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicNPC : MonoBehaviour
+{
+    private Material _material;
+    private SpriteRenderer _sprite;
+
+    void Start()
+    {
+        _sprite = GetComponent<SpriteRenderer>();
+        _material = _sprite.material;
+        _material.SetTexture("_texture", _sprite.sprite.texture);
+    }
+}
